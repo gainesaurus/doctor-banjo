88eade07244dbf3ba3799a893a80024439027701
gainey.michael <gainey.michael@gmail.com>
2023-08-29 09:53:25 -0700

    feat: navbar links and styling

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,9 +2,16 @@ import styles from './Navbar.module.css';
 
 function Navbar() {
   return (
-      <div className={styles.container}>
-        
-      </div>
+      <header className={styles.container}>
+        <h2 className={styles.links}>Doctor Banjo</h2>
+        <a href="#upcoming" className={styles.links}>Upcoming</a>
+        <a href="#banjo" className={styles.links}>Banjo</a>
+        <a href="#guitar" className={styles.links}>Guitar</a>
+        <a href="#classical" className={styles.links}>Classical</a>
+        <a href="#about" className={styles.links}>About</a>
+        <a href="#booking" className={styles.links}>Booking</a>
+        <a href="#patron" className={styles.links}>Become a Patron!</a>
+      </header>
   );
 }
 
