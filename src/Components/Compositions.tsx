f1defd99822ebd99dcb079a702ae86d342b0a019
gainey.michael <gainey.michael@gmail.com>
2026-01-02 01:40:20 -0800

    update Compositions, font

--- src/Components/Compositions.tsx
+++ src/Components/Compositions.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react';
 import styles from './Compositions.module.css';
+import { projects } from '../data';
 
 const Compositions = () => {
 
   return (
     <section className={styles.container} id="compositions" >
-      <p>Compositions by the Doctor.</p>
-      <div className={styles.compEntry}>
-        <div className={styles.compTitle}>Title</div>
-        <div className={styles.pdfLink}>pdf</div>
-        <div className={styles.audioLink}>audio</div>
-        <div className={styles.videoLink}>video</div>
-      </div>      
+     {projects.map(project =>
+        <section className={styles.list}>
+          <h3 className={styles.listTitle}>{project.listTitle}</h3>
+          {project.samples.map(sample => 
+            <div className={styles.compEntry}>
+              <h3 className={styles.compTitle}>{sample.title}</h3>
+              <div className={styles.compMaterials}>
+                <object className={styles.pdfLink} data={`${sample.pdfLink}.pdf#page=3`} />
+                <div className={styles.audioList}>
+                  {sample.audioLinks.map(audioLink =>
+                    <audio controls className={styles.audioLink}>
+                      <source src={audioLink} type="audio/mpeg"/ >
+                      Your browser does not support the audio tag.
+                    </audio>
+                  )}
+                </div>
+
+                {sample.videoLink ?
+                  <div className={styles.videoLink}>video</div>
+                  :
+                  null
+                }
+                
+              </div>
+            </div>
+          )}
+        </section>
+      )}
     </section>
   )
 }
---
+++ src/data.tsx
@@ -0,0 +1,94 @@
+export const projects = [
+  {
+    listTitle: "Compositions by The Doctor",
+    samples: [
+      {
+        title: "Breakdowns",
+        pdfLink: "/assets/compositions/Breakdowns/Breakdowns",
+        audioLinks: [
+          "/assets/compositions/Breakdowns/BreakdownI.mp3",
+          "/assets/compositions/Breakdowns/BreakdownII.mp3",
+          "/assets/compositions/Breakdowns/BreakdownIII.mp3",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "Drifting on the Wild Wind",
+        pdfLink: "/assets/compositions/Drifting/DriftingOnTheWildWind",
+        audioLinks: [
+          "/assets/compositions/Drifting/DriftingOnTheWildWind.mp3",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "Expanse",
+        pdfLink: "/assets/compositions/Expanse/Expanse11",
+        audioLinks: [
+          "/assets/compositions/Expanse/Expanse11.aif",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "Funnel Cloud",
+        pdfLink: "/assets/compositions/FunnelCloud/FunnelCloud",
+        audioLinks: [
+          "/assets/compositions/FunnelCloud/FunnelCloud.mp3",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "Iago",
+        pdfLink: "/assets/compositions/Iago/Iago",
+        audioLinks: [
+          "/assets/compositions/Iago/Iago.mp3",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "North Country Dawn",
+        pdfLink: "/assets/compositions/NorthCountry/NorthCountryDawn",
+        audioLinks: [
+          "/assets/compositions/NorthCountry/NorthCountryDawn.aiff",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "Suite from No Sleep for the Wicked",
+        pdfLink: "/assets/compositions/NoSleep/NoSleepSuite",
+        audioLinks: [
+          "/assets/compositions/NoSleep/SuiteMvtI.mp3",
+          "/assets/compositions/NoSleep/SuiteMvtIV.mp3",
+          "/assets/compositions/NoSleep/SuiteMvtV.mp3",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "Oh Moon",
+        pdfLink: "/assets/compositions/OhMoon/OhMoon",
+        audioLinks: [
+          "/assets/compositions/OhMoon/OhMoon.mp3",
+        ],
+        videoLink: "",
+      },
+
+      {
+        title: "The Selfish Giant",
+        pdfLink: "/assets/compositions/SelfishGiant/SelfishGiant",
+        audioLinks: [
+          "/assets/compositions/SelfishGiant/SelfishGiantI.mp3",
+          "/assets/compositions/SelfishGiant/SelfishGiantII.mp3",
+          "/assets/compositions/SelfishGiant/SelfishGiantIII.mp3",
+        ],
+        videoLink: "",
+      },
+
+    ]
+  }
+];
